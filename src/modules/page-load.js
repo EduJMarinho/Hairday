f7c48084908e1fcfacfb6c4e3552e0f10dffa931
Edu <marinhojcc@hotmail.com>
2025-08-03 23:42:33 -0300

    README

--- src/modules/page-load.js
+++ src/modules/page-load.js
@@ -3,20 +3,3 @@ import { schedulesDay } from "./schedules/load.js";
 document.addEventListener("DOMContentLoaded", function(){
   schedulesDay()
 });
-
-
-
-
-
-
-
-
-
-
-
-
-// import { schedulesDay } from "./schedules/load.js";
-
-// document.addEventListener("DOMContentLoaded", function(){
-//   schedulesDay()
-// });
